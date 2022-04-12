378f57a801e633f54200847472afb1cbd5fae4ca
akshay <gravity.akshay@gmail.com>
2022-04-13 01:57:37 +0530

    Add changes

--- src/client/js/URLChecker.js
+++ src/client/js/URLChecker.js
@@ -1,6 +1,6 @@
 function checkForURLvalidity(inputURL) {
     let res = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-        return (res !== null)
+        return (res !== null);
 }
 
 export { checkForURLvalidity }
